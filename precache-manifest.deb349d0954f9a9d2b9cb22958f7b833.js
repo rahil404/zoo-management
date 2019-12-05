586c2fe50326a3084b64397de69b43dc5223aaeb
rahil404 <r.7arshad@gmail.com>
2019-12-05 09:39:00 +0100

    Updates

---
+++ precache-manifest.deb349d0954f9a9d2b9cb22958f7b833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a42869786dcfddec5bb1b1a93765ad4",
+    "url": "/zoo-management/index.html"
+  },
+  {
+    "revision": "3e96d8f01dc2dc455a5c",
+    "url": "/zoo-management/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a5b2edc97e2f2df0e1b7",
+    "url": "/zoo-management/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3e96d8f01dc2dc455a5c",
+    "url": "/zoo-management/static/js/2.6cce3797.chunk.js"
+  },
+  {
+    "revision": "a5b2edc97e2f2df0e1b7",
+    "url": "/zoo-management/static/js/main.72d3ff2d.chunk.js"
+  },
+  {
+    "revision": "4c2d327d2798c73d71b7",
+    "url": "/zoo-management/static/js/runtime-main.882e39b7.js"
+  },
+  {
+    "revision": "40959fb57b03af6cf8aefb23450e0622",
+    "url": "/zoo-management/static/media/zoo5.40959fb5.jpg"
+  }
+]);
